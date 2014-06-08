1a51116a5730dd63f3209b31f2f9fcea744d6701
jurbi <bideaweegolfer@ymail.com>
2014-06-08 17:04:07 -0400

    added read file from internet

--- plot1.R
+++ plot1.R
@@ -1,4 +1,12 @@
-hpc <- read.csv("household_power_consumption.txt", sep=";", header=TRUE, na.string="?", stringsAsFactors=F)
+
+# read file from internet
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+hpc <- read.csv(unz(temp, "household_power_consumption.txt"), 
+                 sep=";", header=TRUE, na.string="?", stringsAsFactors=F)
+unlink(temp)
+
+# convert variables to true date and datetime
 hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
 hpc$DateTime <- strptime(paste(hpc$Date, hpc$Time), "%Y-%m-%d %H:%M:%S")
 hpc <- hpc[hpc$Date == "2007-02-01" | hpc$Date == "2007-02-02", ]
